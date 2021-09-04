2da7074fcee8eb963f3d47b7adf210753a7b5277
Krishna Kaushik <46679308+krishnakaushik25@users.noreply.github.com>
2021-09-04 18:18:59 +0530

    Update config.js

--- merng-server/config.js
+++ merng-server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB:
-    "mongodb+srv://krishna_11:Sarada@2523@cluster0.fpq7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://krishna_11:mc8jbgXv3Fiiq3Cg@cluster0.fpq7t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     
   SECRET_KEY: 'some very secret key'
 };
