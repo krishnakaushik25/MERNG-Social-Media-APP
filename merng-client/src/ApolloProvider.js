86efef146cd9209227ccc8dae92213f580dd2957
Krishna Kaushik <46679308+krishnakaushik25@users.noreply.github.com>
2021-09-04 18:21:22 +0530

    Update ApolloProvider.js

--- merng-client/src/ApolloProvider.js
+++ merng-client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://sleepy-mountain-67646.herokuapp.com/'
+  uri: 'https://merng-server-11.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
